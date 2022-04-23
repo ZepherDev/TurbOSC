07a6a1098046a0c84c9ad0f3587be53dec5e1fef
Ben <benaclejames@gmail.com>
2022-04-23 16:07:22 -0700

    Initial Commit!!

---
+++ TurbOSC/IKSync.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using MelonLoader;
+using OscCore;
+using VRC.SDK3.Avatars.ScriptableObjects;
+
+namespace TurbOSC
+{
+    public static class IKSync
+    {
+        public static void ReadValues(OscMessageValues values)
+        {
+            List<string> recvValues = new List<string>();
+
+            for (int i = values.Offsets[0]; i < values.m_SharedBuffer.Length;)
+            {
+                List<byte> currParam = new List<byte>();
+                while (values.m_SharedBuffer[i] != 0)
+                {
+                    currParam.Add(values.m_SharedBuffer[i]);
+                    i++;
+                }
+
+                // We can assume that the next byte is a null, because we know that the last byte of the message is a null
+                i++;
+
+                if (currParam.Count == 0) continue;
+                
+                recvValues.Add(Encoding.UTF8.GetString(currParam.ToArray()));
+            }
+
+            foreach (var paramName in recvValues)
+            {
+                var param = ParamLib.ParamLib.FindParam(paramName, VRCExpressionParameters.ValueType.Float);
+                if (param[0] == null) continue;
+                ParamLib.ParamLib.PrioritizeParameter(param[0]);
+            }
+        }
+    }
+}
---
+++ TurbOSC/Main.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using MelonLoader;
+using OscCore;
+using UnityEngine;
+using Main = TurbOSC.Main;
+
+[assembly: MelonGame("VRChat", "VRChat")]
+[assembly: MelonInfo(typeof(Main), "TurbOSC", "1.0", "ZepherDev")]
+
+namespace TurbOSC
+{
+    public class Main : MelonMod
+    {
+        
+        private MonoBehaviourPublicInStObInOs_dOsObBoAcUnique CachedOscMidiController = null;
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            if (CachedOscMidiController != null && CachedOscMidiController.field_Private_OscServer_0 != null) return;
+            
+            var result =
+                GameObject.FindObjectOfType<MonoBehaviourPublicInStObInOs_dOsObBoAcUnique>();
+            
+            if (result == null || result.field_Private_OscServer_0 == null)
+                return;
+
+            CachedOscMidiController = result;
+            var server = CachedOscMidiController.field_Private_OscServer_0;
+            
+            server.TryAddMethod("/turbosc/iksync", 
+                new Action<OscMessageValues>(IKSync.ReadValues));
+            
+            MelonLogger.Msg("TurbOSC Initialized!");
+
+        }
+    }
+}
